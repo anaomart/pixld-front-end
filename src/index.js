a99907f2ada267cbdb131102a81c089def4e374b
anaomart <anaomart@gmail.com>
2023-05-03 13:05:41 +0300

    add hashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import UserContextProvider from './context/UserContext';
 const root = ReactDom.createRoot(document.getElementById('root'))
@@ -10,11 +10,11 @@ const TOKEN = process.env.REACT_APP_GOOGLE_API_TOKEN_ID
 root.render(
     <GoogleOAuthProvider clientId={TOKEN}>
         <UserContextProvider>
-            <BrowserRouter >
+            <HashRouter >
 
                 <App />
 
-            </BrowserRouter>
+            </HashRouter>
         </UserContextProvider>
     </GoogleOAuthProvider>
 
