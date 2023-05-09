4692f057a86dfd20b4ede2ed7fab5f399cb61502
anaomart <anaomart@gmail.com>
2023-05-10 02:50:55 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import './index.css'
-import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import UserContextProvider from './context/UserContext';
-const root = ReactDom.createRoot(document.getElementById('root'))
-const TOKEN = process.env.REACT_APP_GOOGLE_API_TOKEN_ID
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import UserContextProvider from "./context/UserContext";
+const root = ReactDom.createRoot(document.getElementById("root"));
+const TOKEN = process.env.REACT_APP_GOOGLE_API_TOKEN_ID;
 root.render(
-    <GoogleOAuthProvider clientId={TOKEN}>
-        <UserContextProvider>
-            <HashRouter >
-
-                <App />
-
-            </HashRouter>
-        </UserContextProvider>
-    </GoogleOAuthProvider>
-
-)
+  <GoogleOAuthProvider clientId={TOKEN}>
+    <UserContextProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </UserContextProvider>
+  </GoogleOAuthProvider>
+);
